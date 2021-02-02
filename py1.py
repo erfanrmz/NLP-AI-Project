e874b8ec3aae187277597c1529ca1614944b7ff5
Erfan Ramezani <erfanrmz@gmail.com>
2021-02-03 01:30:15 +0330

    reading from trainset and making dictionary for every poet

--- py1.py
+++ py1.py
@@ -1,17 +1,25 @@
-dictionary = dict()
-def word_count(str):
-    words = str.split()
-    for word in words:
-        if word in dictionary:
-            dictionary[word] += 1
-        else:
-            dictionary[word] = 1
+def getTrainSet(file):
+    dictionary = dict()
+    while True:
+        fs = file.readline()
+        if len(fs) == 0:
+            break
+        words = fs.split()
+        for word in words:
+            if word in dictionary:
+                dictionary[word] += 1
+            else:
+                dictionary[word] = 1
+    return dictionary
 
-    return
-f = open("hello1.txt", "r",encoding='UTF-8')
-while True:
-    fs = f.readline()
-    if len(fs) == 0:
-        break
-    word_count(fs)
-print(dictionary)
+
+f = open("./train_set/ferdowsi_train.txt", "r", encoding='UTF-8')
+h = open("./train_set/hafez_train.txt", "r", encoding='UTF-8')
+m = open("./train_set/molavi_train.txt", "r", encoding='UTF-8')
+ferdowsiVoc = {key: val for key, val in getTrainSet(f).items() if val > 2}
+hafezVoc = {key: val for key, val in getTrainSet(h).items() if val > 2}
+molaviVoc = {key: val for key, val in getTrainSet(m).items() if val > 2}
+
+print(ferdowsiVoc)
+print(hafezVoc)
+print(molaviVoc)
