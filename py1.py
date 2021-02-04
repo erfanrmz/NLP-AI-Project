5f1db24dcf49237c714e9087ca80b33d7c89c7ff
Erfan Ramezani <erfanrmz@gmail.com>
2021-02-04 03:44:19 +0330

    back-off modeling Added

--- py1.py
+++ py1.py
@@ -1,3 +1,7 @@
+landa1 = 0.1
+landa2 = 0.5
+landa3 = 0.4
+epsilon = 0.3
 def getTrainSetUnigram(path):
     file = open(path, "r", encoding='UTF-8')
     dictionary = dict()
@@ -39,11 +43,10 @@ class UnigramModel:
 
     def unigramWordProb(self, word):
         wordNo = self.dictionary.get(word, 0)
-        # print(wordNo)
         if wordNo == 0 or self.wordsSize == 0:
             return 0
         else:
-            return float(wordNo) / float(self.wordsSize)
+            return float(wordNo) / float(self.wordsSize - self.dictionary.get("<s>") - self.dictionary.get("</s>"))
 
     def unigramSentenceProb(self, sentence):
         probSum = 1
@@ -61,7 +64,6 @@ class BigramModel(UnigramModel):
     def bigramWordProb(self, preWord, word):
         WordsNo = self.Bidictionary.get((preWord, word), 0)
         WordNo = self.dictionary.get(preWord, 0)
-        print(float(WordsNo) / float(WordNo))
         if WordsNo == 0 or self.dictionary.get(word, 0) == 0:
             return 0
         return float(WordsNo) / float(WordNo)
@@ -77,6 +79,14 @@ class BigramModel(UnigramModel):
             preWord = word
         return probSum
 
+    def backOffWordProb(self, preWord, word):
+        print((self.bigramWordProb(preWord, word) * landa3))
+        print((self.unigramWordProb(word) * landa2))
+        print((landa1 * epsilon))
+        return (self.bigramWordProb(preWord, word) * landa3) + (self.unigramWordProb(word) * landa2) + (landa1 * epsilon)
+
+
+
 
 
 ferdos = BigramModel("hello1.txt")
@@ -86,7 +96,10 @@ print(ferdos.Bidictionary)
 # print(ferdos.bigramWordProb("a", "a"))
 # print(ferdos.bigramWordProb("a", "b"))
 # print(ferdos.bigramWordProb("b", "b"))
-
-print(ferdos.bigramSentenceProb("b a a"))
-print("yes")
+# print(ferdos.unigramWordProb("a"))
+# print(ferdos.unigramWordProb("b"))
+# print(ferdos.bigramWordProb("b", "a"))
+print(ferdos.backOffWordProb("b", "a"))
+# print(ferdos.bigramSentenceProb("b a a"))
+# print("yes")
 # print(ferdos.dictionary)
